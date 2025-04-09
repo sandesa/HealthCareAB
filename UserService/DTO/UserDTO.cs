7a4da50242da1e535a456f6d6ae1ffb4a5b3c39f
Sandesa <samuel.sandenas123@gmail.com>
2025-04-09 09:30:59 +0200

    Add UserService.Tests project and enhance UserDTO
    - Introduced `UserService.Tests` project in `HealthCareAB.sln` with necessary configurations.
    - Added `DateOfBirth` property to `UserDTO` for user date of birth storage.
    - Implemented unit tests in `UserServiceControllerTests.cs` for API functionality, including user retrieval, creation, updating, deletion, and validation.
    - Set up mock for `IValidationRepository` to simulate password verification in tests.
    - Ensured tests validate expected outcomes for user operations.

--- UserService/DTO/UserDTO.cs
+++ UserService/DTO/UserDTO.cs
@@ -7,6 +7,7 @@
         public string? PhoneNumber { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
         public List<string>? UserType { get; set; }
         public List<string>? UserAccountType { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
