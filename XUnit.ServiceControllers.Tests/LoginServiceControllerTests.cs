c92ede88dc910aa7c983b55478455264cf7b509e
Sandesa <samuel.sandenas123@gmail.com>
2025-04-11 03:32:59 +0200

    Refactor solution structure and update test projects
    - Removed `UserService.Tests` project and added `XUnit.ServiceControllers.Tests`.
    - Updated solution configurations to reflect project changes.
    - Modified `LoginController.cs` to use a generic `[HttpPost]` attribute.
    - Enhanced `LoginResponse` class with new `Token` and `Message` properties.
    - Updated `LoginRepository.cs` to include `Token` in login responses.
    - Removed `UserService.Tests.csproj` and its associated files.
    - Refactored `UserServiceControllerTests.cs` to include necessary using directives and uncommented test methods.
    - Added new `LoginServiceControllerTests` class with login test functionality.
    - Created `XUnit.ServiceControllers.Tests.csproj` with required properties and references.

---
+++ XUnit.ServiceControllers.Tests/LoginServiceControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using LoginService;
+using LoginService.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+
+namespace XUnit.ServiceControllers.Tests
+{
+    public class LoginServiceControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public LoginServiceControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task LoginUser_ReturnsUnauthorized()
+        {
+            LoginRequest request = new()
+            {
+                Email = "test1.testsson@gmail.com",
+                Password = "123"
+            };
+            var response = await _client.PostAsJsonAsync("/api/login", request);
+            //response.EnsureSuccessStatusCode();
+            //var jsonResponse = await response.Content.ReadFromJsonAsync<JsonElement>();
+            //var token = jsonResponse.GetProperty("Token").ToString();
+            //var isLoginSuccessful = jsonResponse.GetProperty("IsLoginSuccessful").GetBoolean();
+            //var isConnectedToService = jsonResponse.GetProperty("IsConnectedToService").GetBoolean();
+            //Assert.NotNull(token);
+            //Assert.True(isLoginSuccessful);
+            //Assert.True(isConnectedToService);
+        }
+    }
+
+}
