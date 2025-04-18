a229e0e42034be9050ec6fa4e0bc9c8fede3c512
Sandesa <samuel.sandenas123@gmail.com>
2025-04-18 18:16:45 +0200

    Add JWT, OpenAPI, and Scalar support to GatewayService
    Updated project structure and configuration to include:
    - New package references for JWT authentication, OpenAPI, and Scalar.
    - Dependency injection methods in Program.cs for service setup.
    - Launch settings to enable browser launch with a default URL.
    - JWT configuration in appsettings.json for authentication.
    - Created GatewayController, IGatewayRepository, GatewayRepository, GatewayService, DependenciesConfig, and OpenApiConfig classes for improved architecture and functionality.

---
+++ GatewayService/Controllers/GatewayController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GatewayService.Controllers
+{
+    [ApiController]
+    [Route("api/gateway")]
+    public class GatewayController : Controller
+    {
+
+    }
+}
