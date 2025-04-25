32ec52158eaf1d5e9b82af565ce8e5f1eb2b583c
Sampe <samuel.sandenas123@gmail.com>
2025-04-25 17:08:49 +0200

    Refactor UI and introduce Home component
    - Removed styles from App.css and restructured App.tsx to use the new Home component.
    - Updated index.tsx to render Home in StrictMode.
    - Added Home.css for new layout styles and created Home.tsx for content presentation.
    - Included new image asset, MountinDark.jpg, for demonstration purposes.

--- frontendapp/src/index.tsx
+++ frontendapp/src/index.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Home from './Home.tsx'
 import Navbar from './navbar.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
+    <StrictMode>
+        <Home />
   </StrictMode>,
 )
 
